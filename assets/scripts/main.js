bb88b6391affc016152b31f70fcaf0c617fffdb5
Tung <ductungltt@gmail.com>
2024-01-07 21:30:01 +0700

    new: add main.js

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+const importPartial = (url, partialId) => {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(partialId).innerHTML = html;
+        })
+};
+
+importPartial('../../partials/header.html', 'header');
+importPartial('../../partials/clients.html', 'clients');
+importPartial('../../partials/team.html', 'team');
+importPartial('../../partials/blog.html', 'blog');
+importPartial('../../partials/footer.html', 'footer');
